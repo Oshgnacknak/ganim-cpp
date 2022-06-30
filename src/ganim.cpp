856e39c9cc02f2ca2a86291e50595cb5240a46ad
Oshgnacknak <osh@oshgnacknak.de>
2022-06-30 21:46:37 +0200

    Initial particle having position

--- src/ganim.cpp
+++ src/ganim.cpp
@@ -11,17 +11,34 @@ using namespace osh;
 
 bool running = true;
 
-MV create_translator(MV delta) {
+MV create_translator(MV t) {
 #pragma gpc begin
-    create_translator_delta = mv_from_array(delta);
+    create_translator_t = mv_from_array(t);
 #pragma clucalc begin
-    ? create_translator_res = 1 - 0.5*create_translator_delta*einf;
+    ? create_translator_res = 1 - 0.5 * create_translator_t * einf;
 #pragma clucalc end
     MV mv;
     mv = mv_to_array(create_translator_res);
     return mv;
 #pragma gpc end
 }
+
+struct Particle {
+    MV motor = create_translator(
+        create_vector(WIDTH/2.0, HEIGHT/2.0));
+    MV vel;
+
+    MV position() {
+        return create_point() << motor;
+    }
+};
+
+// function dState([M,B]) {
+//   return  [
+//     -0.5 * M * B,
+//     -0.5 * (B.Dual*B - B*B.Dual).UnDual
+//   ]
+// }
 
 int main() {
     open_window();
@@ -30,6 +47,8 @@ int main() {
     });
 
     double last = current_time();
+
+    Particle particle;
 
     MV point = create_point(WIDTH/2 - 60, HEIGHT/2);
 
@@ -55,6 +74,9 @@ int main() {
         begin_render();
         draw_point(point.x(), point.y(), RED);
         draw_point(rotated.x(), rotated.y(), GREEN);
+
+        MV pos = particle.position();
+        draw_point(pos.x(), pos.y(), BLUE);
         end_render();
 
         last = now;
