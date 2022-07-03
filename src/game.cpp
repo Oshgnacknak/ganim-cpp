07530db5ef150527b44030a26f55a1a818dcca01
Oshgnacknak <osh@oshgnacknak.de>
2022-07-03 21:37:28 +0200

    Movement like in PGADyn

--- src/game.cpp
+++ src/game.cpp
@@ -18,7 +18,7 @@ struct Particle {
     MV motor = create_motor(
         create_point(0, 0),
         create_point(WIDTH/2.0, HEIGHT/2.0));
-    MV vel = !create_vector(0, 0);
+    MV vel = !create_vector(150, -100);
 
     MV position() {
         MV pos = !(create_point() << motor);
@@ -28,7 +28,7 @@ struct Particle {
 
     void update(double dt) {
         MV dm = -0.5 * motor * vel;
-        MV dv = !(0.5 * (!vel * vel - vel * !vel));
+        MV dv = !(- 0.5 * (!vel * vel - vel * !vel));
         motor += dt * dm;
         vel += dt * dv;
     }
